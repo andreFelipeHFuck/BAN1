5fe530e39a09f9240dbe3e511fd33e52ed846dc1
joao30045 <74935131+joao30045@users.noreply.github.com>
2023-11-12 10:58:17 -0300

    Add files via upload

--- projeto/dados/Kits.java
+++ projeto/dados/Kits.java
@@ -1,48 +1,43 @@
 package projeto.dados;
 
 public class Kits {
+    
     private int codKit;
+    private int codKitProduto;
     private int codProduto;
     private int quantidadeProduto;
 
-
-    public Kits(int codKit, int codProduto, int quantidadeProduto){
+    public Kits(int codKit, int codKitProduto, int codProduto, int quantidadeProduto) {
         this.codKit = codKit;
+        this.codKitProduto = codKitProduto;
         this.codProduto = codProduto;
         this.quantidadeProduto = quantidadeProduto;
     }
-
-    public Kits(){
-
-    }
-
     public int getCodKit() {
         return codKit;
     }
-
     public void setCodKit(int codKit) {
         this.codKit = codKit;
     }
-
+    public int getCodKitProduto() {
+        return codKitProduto;
+    }
+    public void setCodKitProduto(int codKitProduto) {
+        this.codKitProduto = codKitProduto;
+    }
     public int getCodProduto() {
         return codProduto;
     }
-
     public void setCodProduto(int codProduto) {
         this.codProduto = codProduto;
     }
-
     public int getQuantidadeProduto() {
         return quantidadeProduto;
     }
-
     public void setQuantidadeProduto(int quantidadeProduto) {
         this.quantidadeProduto = quantidadeProduto;
     }
-
-    @Override
     public String toString() {
-        // TODO Auto-generated method stub
-        return super.toString();
+        return "codKit:" + codKit + " codKitProduto:" + codKitProduto + " codProduto:" + codProduto + " quantidadeProduto:" + quantidadeProduto;
     }
 }
--- projeto/dados/Produtos.java
+++ projeto/dados/Produtos.java
@@ -1,78 +1,60 @@
 package projeto.dados;
 
 public class Produtos {
+    
     private int codProduto;
-    private float precoUnitario;
-    private float precoUnitarioCompra;
+    private int precoUnitVenda;
+    private int precoUnitCompra;
     private String descricao;
     private int quantidade;
     private String datasheet;
 
-
-    public Produtos(int codCliente, float precoUnitario, float precoUnitarioCompra, String descricao, int quantidade, String datasheet){
+    public Produtos(int codProduto, int precoUnitVenda, int precoUnitCompra, String descricao, int quantidade, String datasheet) {
         this.codProduto = codProduto;
-        this.precoUnitario = precoUnitario;;
-        this.precoUnitarioCompra = precoUnitarioCompra;
+        this.precoUnitVenda = precoUnitVenda;
+        this.precoUnitCompra = precoUnitCompra;
         this.descricao = descricao;
         this.quantidade = quantidade;
         this.datasheet = datasheet;
     }
-
-    public Produtos(){
-
-    }
-
     public int getCodProduto() {
         return codProduto;
     }
-
     public void setCodProduto(int codProduto) {
         this.codProduto = codProduto;
     }
-
-    public float getPrecoUnitario() {
-        return precoUnitario;
+    public int getPrecoUnitVenda() {
+        return precoUnitVenda;
     }
-
-    public void setPrecoUnitario(float precoUnitario) {
-        this.precoUnitario = precoUnitario;
+    public void setPrecoUnitVenda(int precoUnitVenda) {
+        this.precoUnitVenda = precoUnitVenda;
     }
-
-    public float getPrecoUnitarioCompra() {
-        return precoUnitarioCompra;
+    public int getPrecoUnitCompra() {
+        return precoUnitCompra;
     }
-
-    public void setPrecoUnitarioCompra(float precoUnitarioCompra) {
-        this.precoUnitarioCompra = precoUnitarioCompra;
+    public void setPrecoUnitCompra(int precoUnitCompra) {
+        this.precoUnitCompra = precoUnitCompra;
     }
-
     public String getDescricao() {
         return descricao;
     }
-
     public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
-
     public int getQuantidade() {
         return quantidade;
     }
-
     public void setQuantidade(int quantidade) {
         this.quantidade = quantidade;
     }
-
     public String getDatasheet() {
         return datasheet;
     }
-
     public void setDatasheet(String datasheet) {
         this.datasheet = datasheet;
     }
-
-    @Override
     public String toString() {
-        // TODO Auto-generated method stub
-        return super.toString();
+        return "codProduto:" + codProduto + " precoUnitVenda:" + precoUnitVenda + " precoUnitCompra:" + precoUnitCompra + " descricao:" + descricao
+         + " quantidade:" + quantidade + " datasheet:" + datasheet;
     }
 }
